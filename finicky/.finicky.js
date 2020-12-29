93a4949f7f8c346e8752509a2e6cb0fe7daa11d2
Patrick Tsai <patrick@simplypatrick.com>
2020-12-29 15:56:50 -0800

    Update finicky

--- finicky/.finicky.js
+++ finicky/.finicky.js
@@ -2,12 +2,10 @@ module.exports = {
   defaultBrowser: "Firefox",
   handlers: [
     {
-      // Open apple.com and example.org urls in Safari
       match: finicky.matchHostnames(["medium.com"]),
       browser: "Firefox"
     },
     {
-      // Open apple.com and example.org urls in Safari
       match: finicky.matchHostnames([/.+\.aliexpress\.com/]),
       browser: "Google Chrome"
     }
